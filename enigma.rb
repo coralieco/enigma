24c820c3ad83c101af38b7439985ffa05a041e9f
Coralie Collignon <coralie.collignon@gmail.com>
2017-08-10 14:12:40 +0200

    first commit

---
+++ enigma.rb
@@ -0,0 +1,6 @@
+class Enigma
+
+  def encrypt(word)
+
+  end
+end
---
+++ rotor.rb
@@ -0,0 +1,44 @@
+class Rotor
+
+  CONNEXIONS_TABLE =
+    {
+      'a': 'c',
+      'b': 'b',
+      'c': 'a',
+      'd': 'f',
+      'e': 'e',
+      'f': 'd',
+
+      'g': 'i',
+      'h': 'h',
+      'i': 'g',
+      'j': 'l',
+      'k': 'k',
+      'l': 'j',
+
+      'm': 'o',
+      'n': 'n',
+      'o': 'm',
+      'p': 'r',
+      'q': 'q',
+      'r': 'p',
+
+      's': 'v',
+      't': 'u',
+      'u': 't',
+      'v': 's',
+      'w': 'z',
+      'x': 'y',
+      'y': 'x',
+      'z': 'w'
+    }
+  # on tape une lettre
+  # STEP1: elle passe dans le table des connexions
+  # STEP2: elle passe successivement dans trois rotors
+  # STEP3: elle passe dans le réflecteur
+  # NB: Après chaque lettre tapée, le premier rotor tourne d'un cran. Quand il est revenu à sa position initiale, le deuxième rotor se met à tourner.
+
+  def rotor_rotation
+
+  end
+end
